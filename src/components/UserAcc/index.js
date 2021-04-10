ef181a43ab88f1f270461162f6a72a715ae516cb
Tumise <oladele.alade@gmail.com>
2021-04-10 21:27:35 +0100

    chore: (component) added component setup for user account

--- src/components/UserAcc/index.js
+++ src/components/UserAcc/index.js
@@ -2,7 +2,18 @@ import React from "react";
 
 
 function UserAcc(){
-    return <React.Fragment></React.Fragment>
+    return <React.Fragment>
+       <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                One of three columns
+                </div>
+                <div class="col-8">
+                One of three columns
+                </div>
+            </div>
+        </div>
+    </React.Fragment>
 }
 
 export default UserAcc;
