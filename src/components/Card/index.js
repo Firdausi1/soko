f95a6ae68faf58c1c4c38fe1550db5e6376c3765
Tumise <oladele.alade@gmail.com>
2021-04-10 21:58:17 +0100

    chore: (card) added style for card item

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,9 +3,20 @@ import style from "./style.module.css";
 import clsx from 'clsx'
 function Card(){
     return <div>
-        <div className={`${style["group"]} ${style["bold_text"]}`}>
+        <div className={clsx (style.group, style.bold_text)}>
             <img src="https://ik.imagekit.io/ikmedia/bucket.png?tr=w-500,dpr-2" className={style["item__img"]} alt=""/>
             <div>Target</div>
+        </div>
+        <div>
+            <div className={clsx(style.group,style.item__details)}>
+                <p className={clsx(style.bold_text)}> Order #ID</p>
+                <p className={clsx(style.bold_text)}> UGX 12,700</p>
+            </div>
+            <div className={clsx(style.group,style.item__details)}>
+                <p> 3 Items</p>
+                <p> 25/02/2021, 01:55 PM</p>
+            </div>
+
         </div>
     </div>
 }
