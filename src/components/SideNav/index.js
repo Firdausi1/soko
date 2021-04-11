1afe50fa4ab9d83e8d5b8d46afe2fd975893dba4
Tumise <oladele.alade@gmail.com>
2021-04-11 12:03:22 +0100

    ft: (sideNav) added media responsiveness

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -2,10 +2,10 @@ import clsx from "clsx"
 import React from "react"
 import style from "./style.module.css"
 function SideNav() {
+    
   return (
     <div className={style.background}>
-      <ul className={clsx(style.clear, style.pointer)}>
-          
+      <ul className={clsx(style.clear, style.pointer, style.nav)}>
         <li className={style.group}>
           <i className={clsx("bi bi-card-text", style.icon)}></i>
           <span className={style.icon__text}>My Order</span>
@@ -13,7 +13,7 @@ function SideNav() {
 
         <li className={style.group}>
           <i className={clsx("bi bi-card-text", style.icon)}></i>
-          <span className={style.icon__text}>My Order</span>
+          <span className={style.icon__text}>My Addresses</span>
         </li>
       </ul>
     </div>
