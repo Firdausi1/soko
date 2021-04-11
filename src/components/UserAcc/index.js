750e6918e5b41d4572904d88888e8a12a7bf2395
Tumise <oladele.alade@gmail.com>
2021-04-11 09:57:32 +0100

    rf: (userAcc) formatted with prettier

--- src/components/UserAcc/index.js
+++ src/components/UserAcc/index.js
@@ -1,29 +1,28 @@
-import React from "react";
-import Card from "../Card";
+import React from "react"
+import Card from "../Card"
 import "../../App.css"
-import style from "./style.module.css";
-import clsx from 'clsx'
-import DropDown from "../DropDown";
+import style from "./style.module.css"
+import clsx from "clsx"
+import DropDown from "../DropDown"
 
-function UserAcc(){
-    return  <div className={clsx("container",style.inner_space)}>
-            <div className="row">
-                <div className="col px-4">
-                One of three columns
-                </div>
-                <div className="col-sm-12 col-md-9">
-                <div className={clsx(style.group ,"gutter-x")}>
-                    <h5>Showing all orders</h5>
-                    <DropDown/>
-                </div>
-                    <div className={clsx(style.center ,"gutter-xy")}>
-                        <Card/>
-                        <Card/>
-                    </div>   
-                </div>
-            </div>
+function UserAcc() {
+  return (
+    <div className={clsx("container", style.inner_space)}>
+      <div className="row">
+        <div className="col px-4">One of three columns</div>
+        <div className="col-sm-12 col-md-9">
+          <div className={clsx(style.group, "gutter-x")}>
+            <h5 className="bold_text">Showing all orders</h5>
+            <DropDown />
+          </div>
+          <div className={clsx(style.center, "gutter-xy")}>
+            <Card />
+            <Card />
+          </div>
         </div>
-
+      </div>
+    </div>
+  )
 }
 
-export default UserAcc;
+export default UserAcc
