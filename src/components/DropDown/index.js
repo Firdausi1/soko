65e555f12789db6f653d65c7d748d5a571813fd1
Tumise <oladele.alade@gmail.com>
2021-04-11 09:42:53 +0100

    ft: (dropDown) added drop down item

--- src/components/DropDown/index.js
+++ src/components/DropDown/index.js
@@ -3,14 +3,41 @@ import clsx from 'clsx';
 import style from "./style.module.css"
 
 function DropDown(){
-    return <div className="btn-group dropstart">
-    <button className={clsx(style.border,"btn"  ,"btn-sm" ,"dropdown-toggle")} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-      Filter
+    const items = ["High to Low","New Release"]
+
+    function DropDownItem({name}){
+        return  <li className={clsx("dropdown-item",style.pointer)}>{name}</li>
+    }
+
+    return <div className={clsx("btn-group",style.border,style.space)}>
+    <button type="button" className="btn btn-sm  dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+      <span className="visually-hidden">Toggle Dropdown</span>
     </button>
+    <button className="btn btn-sm bold_text" type="button">
+      Filters
+    </button>
+    
     <ul className="dropdown-menu">
-      ...
+            {items.map((item)=><DropDownItem name={item}/>)}
+   
     </ul>
   </div>
+    
+    
+
+
+
 }
+
+//     <div className="btn-group dropstart">
+//     <button className={clsx(style.border,"btn"  ,"btn-sm" ,"dropdown-toggle")} type="button" data-bs-toggle="dropdown" aria-expanded="false">
+//       Filter
+//     </button>
+//     <ul className="dropdown-menu">
+//       ...
+//     </ul>
+//   </div>
+
+
 
 export default DropDown
