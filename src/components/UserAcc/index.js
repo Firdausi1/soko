fd673fd793b7e1d7d8d0c9242eb42c2bfe01e6df
Tumise <oladele.alade@gmail.com>
2021-04-11 10:56:43 +0100

    ft: (useAcc) added min height and border

--- src/components/UserAcc/index.js
+++ src/components/UserAcc/index.js
@@ -7,11 +7,13 @@ import DropDown from "../DropDown"
 
 function UserAcc() {
   return (
-    <div className={clsx("container border", style.inner_space, style.gap)}>
+    <div className={clsx("container border", style.gap, style.min_height)}>
       <div className="row">
-        <div className="col px-4">One of three columns</div>
+        <div className={clsx("col px-4", style["border-y"])}>
+          <div className={clsx(style.inner_space)}> One of three columns</div>
+        </div>
         <div className="col-sm-12 col-md-9">
-          <div className={clsx(style.group, "gutter-x")}>
+          <div className={clsx(style.group, "gutter-x", style.inner_space)}>
             <h5 className="bold_text">Showing all orders</h5>
             <DropDown />
           </div>
