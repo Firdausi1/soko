9ca2ebf950eda423dcfc7f5b48f906321d08e012
Tumise <oladele.alade@gmail.com>
2021-04-11 09:53:06 +0100

    chore: (prettier) setup and formatted with prettier

--- src/components/DropDown/index.js
+++ src/components/DropDown/index.js
@@ -1,43 +1,37 @@
 import React from "react";
-import clsx from 'clsx';
-import style from "./style.module.css"
+import clsx from "clsx";
+import style from "./style.module.css";
 
-function DropDown(){
-    const items = ["High to Low","New Release"]
+function DropDown() {
+  const items = ["High to Low", "New Release"];
 
-    function DropDownItem({name}){
-        return  <li className={clsx("dropdown-item",style.pointer)}>{name}</li>
-    }
+  function DropDownItem({ name }) {
+    return <li className={clsx("dropdown-item", style.pointer)}>{name}</li>;
+  }
 
-    return <div className={clsx("btn-group",style.border,style.space)}>
-    <button type="button" className="btn btn-sm  dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-      <span className="visually-hidden">Toggle Dropdown</span>
-    </button>
-    <button className="btn btn-sm bold_text" type="button">
-      Filters
-    </button>
-    
-    <ul className="dropdown-menu">
-            {items.map((item)=><DropDownItem name={item}/>)}
-   
-    </ul>
-  </div>
-    
-    
+  return (
+    <div className={clsx("btn-group", style.border, style.space)}>
+      <button
+        type="button"
+        className="btn btn-sm  dropdown-toggle dropdown-toggle-split"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        <span className="visually-hidden">Toggle Dropdown</span>
+      </button>
+      <button className="btn btn-sm bold_text" type="button">
+        Filters
+      </button>
 
-
-
+      <ul className="dropdown-menu">
+        {items.map((item) => (
+          <DropDownItem name={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-//     <div className="btn-group dropstart">
-//     <button className={clsx(style.border,"btn"  ,"btn-sm" ,"dropdown-toggle")} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-//       Filter
-//     </button>
-//     <ul className="dropdown-menu">
-//       ...
-//     </ul>
-//   </div>
 
 
-
-export default DropDown
+export default DropDown;
